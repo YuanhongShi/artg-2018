1d85c8dad6cab3153b37145fba230df0e97e7b62
YuanhongShi <31866504+YuanhongShi@users.noreply.github.com>
2018-02-09 16:53:59 -0500

    update the class functions files

---
+++ 00-hubway-project/src/components/mainViz.js
@@ -0,0 +1,56 @@
+import * as d3 from 'd3';
+import Histogram from './Histogram';
+
+import '../style/main.css';
+
+function MainViz(_){
+
+	const timeline = Histogram()
+		.domain([new Date(2013,0,1), new Date(2013,11,31)])	//javascript month from 0 to 11
+		.value(d => d.t0)
+		.thresholds(d3.timeMonth.range(new Date(2013,0,1),new Date(2013,11,31),1))
+		.tickXFormat(d => {
+			return (new Date(d)).toUTCString();
+		})
+		.tickX(3)
+		.tickY(2)
+		.maxY(400);
+
+
+	function exports(data,i){
+		const width = this.clientWidth;
+		const height = this.clientHeight;
+
+		//data transfromation
+		//nest by starting station
+		const tripsByStation = d3.nest()
+			.key(d=>d.station0)
+			.entries(data)
+			.map(d => {
+				return d.values;
+			});	//sort怎么做
+
+		console.log(tripsByStation);
+		//Create a node for each station
+		//call timeline module on each node
+		const stationNodes = d3.select(this)
+			.selectAll('.staion-node')
+			.data(tripsByStation); //update selction of size 0 (initially)
+
+		const stationNodesEnter = stationNodes.enter()
+			.append('div')
+			.classed('station-node', true)
+			.style('width', '300px')
+			.style('height', '180px')
+			.style('float', 'left');
+		stationNodes.exit().remove();
+
+		stationNodesEnter.merge(stationNodes)
+			.each(timeline);
+
+	}
+
+	return exports;
+}
+
+export default MainViz;
--- 00-hubway-project/src/index.js
+++ 00-hubway-project/src/index.js
@@ -1,2 +1,58 @@
 import * as d3 from 'd3';
+import './style/main.css';
+import './style/stationSearch.css';
 
+//Import utility function
+import {parse, parse2} from './utils';	//syntax of import of different types of export
+
+//Import modules
+import Histogram from './components/Histogram';
+//Histogram is the factory function
+import MainViz from './components/mainViz';
+
+const timeline = Histogram()
+	.domain([new Date(2013,0,1), new Date(2017,11,31)])	//javascript month from 0 to 11
+	.value(d => d.t0)
+	.thresholds(d3.timeMonth.range(new Date(2013,0,1),new Date(2017,11,31),1))
+	.tickXFormat(d => {
+		return (new Date(d)).toUTCString();
+	})
+	.tickX(2);
+
+
+
+const activityHistogram = Histogram();
+activityHistogram
+	.thresholds(d3.range(0,24,.25))
+	.domain([0,24])
+	.value(d=>d.time_of_day0)	//只是一个定义函数的格式，并没有call它，真正在用的时候才call到data
+	.tickXFormat(d => {
+				const time = +d;
+				const hour = Math.floor(time);
+				let min = Math.round((time-hour)*60);
+				min = String(min).length === 1? "0"+ min : min;
+				return `${hour}:${min}`
+		})
+	.maxY(1000);	
+	
+const mainViz = MainViz();	//closure
+
+d3.csv('./data/hubway_trips_reduced.csv', parse, (err,trips) => {
+
+	d3.select('#time-of-the-day-main')
+		.datum(trips)
+		.each(activityHistogram);
+
+	d3.select('#timeline-main')
+		.datum(trips)
+		.each(timeline);
+
+	d3.select('.main')
+		.datum(trips)
+		.each(mainViz);
+
+
+
+
+
+});
